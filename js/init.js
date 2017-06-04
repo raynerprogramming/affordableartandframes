46ebcb20ee785bae31cb1bfcaca583daf5ae6a9d
raynerprogramming <raynerprogramming@gmail.com>
2017-06-04 11:39:47 -0500

    clean up header

--- js/init.js
+++ js/init.js
@@ -1,7 +1,9 @@
 (function($){
-  $(function(){
+	$(function(){
 
-    $('.button-collapse').sideNav();
+		$('.button-collapse').sideNav();
+		$("#header").load("header.html"); 
+		$("#footer").load("footer.html"); 
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
