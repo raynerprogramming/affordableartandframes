990e67a6ae5c0a6aee16b600b3fdbde038f3d64f
raynerprogramming <raynerprogramming@gmail.com>
2017-06-04 14:22:48 -0500

    test

--- js/init.js
+++ js/init.js
@@ -3,6 +3,22 @@
 
 		$('.button-collapse').sideNav();
 		$('.parallax').parallax();
+		   $(function(){
+    $('.carousel.carousel-slider').each(function() {
+      var view = $(this);
+      var firstImage = view.find('.carousel-item img').first();
+      var imageHeight = firstImage[0].height;
+      if (imageHeight > 0) {
+     //   view.css('height', imageHeight);
+      }
+      else {
+    //    view.css('height', 400);
+      }
+    });
+    $('.carousel').carousel({full_width: true});
+    $("#header").load("header.html"); 
+    $("#footer").load("footer.html"); 
+  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
