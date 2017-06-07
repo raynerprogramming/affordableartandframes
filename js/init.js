12e0f67c25beb1c5ed68f618a72462bc842dfbe4
raynerprogramming <raynerprogramming@gmail.com>
2017-06-07 18:01:58 -0500

    test

--- js/init.js
+++ js/init.js
@@ -2,19 +2,17 @@
 	$(function(){
 		$('.button-collapse').sideNav();
 		$('.parallax').parallax();
-		$(function(){
-			$('.carousel.carousel-slider').each(function() {
-				var view = $(this);
-				var firstImage = view.find('.carousel-item img').first();
-				var imageHeight = firstImage[0].height;
-				if (imageHeight > 0) {
-					view.css('height', imageHeight);
-				}
-				else {
+		$('.carousel.carousel-slider').each(function() {
+			var view = $(this);
+			var firstImage = view.find('.carousel-item img').first();
+			var imageHeight = firstImage[0].height;
+			if (imageHeight > 0) {
+				view.css('height', imageHeight);
+			}
+			else {
 				//	view.css('height', 400);
-				}
-			});
-			$('.carousel').carousel({full_width: true});
+			}
 		});
+		$('.carousel').carousel({full_width: true});
   }); // end of document ready
 })(jQuery); // end of jQuery name space
