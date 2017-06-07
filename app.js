163e0cc13922b28f9b7e73aaa62c1dcef01a8ef9
raynerprogramming <raynerprogramming@gmail.com>
2017-06-07 18:20:30 -0500

    test

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ var app = angular.module('app',[])
 	$scope.data='test';
 });
 
+app.run(function($rootScope){
+	$rootScope.$on("$includeContentLoaded", function(event, templateName){
+		(function($){
+			$(function(){
+				$('.button-collapse').sideNav();
+				$('.parallax').parallax();
+				$('.carousel.carousel-slider').each(function() {
+					var view = $(this);
+					var firstImage = view.find('.carousel-item img').first();
+					var imageHeight = firstImage[0].height;
+					if (imageHeight > 0) {
+						view.css('height', imageHeight);
+					}
+					else {
+				//	view.css('height', 400);
+			}
+		});
+				$('.carousel').carousel({full_width: true});
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+});
+});
