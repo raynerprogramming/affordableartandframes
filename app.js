68f2f7da4eeeddd75d6fa6cf5a96e554b0f44dae
raynerprogramming <raynerprogramming@gmail.com>
2017-06-07 21:20:05 -0500

    test

--- app.js
+++ app.js
@@ -26,8 +26,13 @@ var app = angular.module('app',[])
 
 });
 
-app.run(function($rootScope){
-  $rootScope.$on("$includeContentLoaded", function(event, templateName){
-     $('.raynerNavBar').sideNav();
-  });
-});
+app.directive('header',function(){
+	return{
+	restrict: 'A', //This menas that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
+        replace: true,
+        templateUrl: "header.html",
+        controller: ['$scope', '$filter', function ($scope, $filter) {
+            // Your behaviour goes here :)
+        }]
+    }
+})
