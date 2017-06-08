6d725c36bda9dfcc36594073581cffbbc3e52632
raynerprogramming <raynerprogramming@gmail.com>
2017-06-07 19:55:43 -0500

    test

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 var app = angular.module('app',[])
 .controller('framingController',function($scope){
 	$scope.data='test';
+
+	$(function () {
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+		$('.carousel.carousel-slider').each(function() {
+			var view = $(this);
+			var firstImage = view.find('.carousel-item img').first();
+			var imageHeight = firstImage[0].height;
+			if (imageHeight > 0) {
+				view.css('height', imageHeight);
+			}
+			else {
+				//	view.css('height', 400);
+			}
+		});
+		$('.carousel').carousel({full_width: true});
+	});
 });
