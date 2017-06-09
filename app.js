ffd3e3aed19e075576c05a6921eda3b64e19f7f8
raynerprogramming <raynerprogramming@gmail.com>
2017-06-09 12:44:13 -0500

    cleanup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = angular.module('app',['ui.router'])
 
 	$scope.init = function(){
 		$(function () {
-			$('.raynerNavBar').sideNav();
+			$('.button-collapse').sideNav();
 			$('.parallax').parallax();
 			$('.carousel.carousel-slider').each(function() {
 				var view = $(this);
@@ -14,9 +14,9 @@ var app = angular.module('app',['ui.router'])
 					view.css('height', imageHeight);
 				}
 				else {
-				//	view.css('height', 400);
-			}
-		});
+					view.css('height', 400);
+				}
+			});
 			$('.carousel').carousel({full_width: true});
 		});
 	}
@@ -24,28 +24,12 @@ var app = angular.module('app',['ui.router'])
 		$scope.init();
 	});
 })
-.controller('RootController',function($scope,$state){
-	$scope.data='Root';
-	//$state.go('home');
-})
 .controller('HomeController',function($scope){
 	$scope.data='Home';
 	$scope.init = function(){
 		$(function () {
-			$('.raynerNavBar').sideNav();
+			$('.button-collapse').sideNav();
 			$('.parallax').parallax();
-			$('.carousel.carousel-slider').each(function() {
-				var view = $(this);
-				var firstImage = view.find('.carousel-item img').first();
-				var imageHeight = firstImage[0].height;
-				if (imageHeight > 0) {
-					view.css('height', imageHeight);
-				}
-				else {
-				//	view.css('height', 400);
-			}
-		});
-			$('.carousel').carousel({full_width: true});
 		});
 	}
 	angular.element(document).ready(function () {
