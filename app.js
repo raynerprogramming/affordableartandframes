5ca468eef6a990e2b0ad913b425497639b3f0737
raynerprogramming <raynerprogramming@gmail.com>
2017-06-08 21:24:07 -0500

    test

--- app.js
+++ app.js
@@ -51,33 +51,7 @@ var app = angular.module('app',['ui.router'])
 	});
 
 })
-.controller('framingController',function($scope){
-	$scope.data='test';
-
-	$scope.init = function(){
-		$(function () {
-			$('.raynerNavBar').sideNav();
-			$('.parallax').parallax();
-			$('.carousel.carousel-slider').each(function() {
-				var view = $(this);
-				var firstImage = view.find('.carousel-item img').first();
-				var imageHeight = firstImage[0].height;
-				if (imageHeight > 0) {
-					view.css('height', imageHeight);
-				}
-				else {
-				//	view.css('height', 400);
-			}
-		});
-			$('.carousel').carousel({full_width: true});
-		});
-	}
-	angular.element(document).ready(function () {
-		$scope.init();
-	});
-
-})
-.controller('RootController',function($scope){
+.controller('RootController',function($scope,$state){
 	$scope.data='Root';
 
 	$scope.init = function(){
@@ -101,9 +75,10 @@ var app = angular.module('app',['ui.router'])
 	angular.element(document).ready(function () {
 		$scope.init();
 	});
+	$state.go('app.home');
 
 })
-.controller('framingController',function($scope){
+.controller('HomeController',function($scope){
 	$scope.data='Home';
 
 	$scope.init = function(){
