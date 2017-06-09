7a64cfa9fff3d53159b0ce68d061a40885ca14f9
raynerprogramming <raynerprogramming@gmail.com>
2017-06-08 22:07:44 -0500

    test

--- app.js
+++ app.js
@@ -1,57 +1,8 @@
 var app = angular.module('app',['ui.router'])
 .controller('framingController',function($scope){
-	$scope.data='test';
-
-	$scope.init = function(){
-		$(function () {
-			$('.raynerNavBar').sideNav();
-			$('.parallax').parallax();
-			$('.carousel.carousel-slider').each(function() {
-				var view = $(this);
-				var firstImage = view.find('.carousel-item img').first();
-				var imageHeight = firstImage[0].height;
-				if (imageHeight > 0) {
-					view.css('height', imageHeight);
-				}
-				else {
-				//	view.css('height', 400);
-			}
-		});
-			$('.carousel').carousel({full_width: true});
-		});
-	}
-	angular.element(document).ready(function () {
-		$scope.init();
-	});
 
 })
-.controller('footerCtrl',function($scope){
-	$scope.data='test';
-
-	$scope.init = function(){
-		$(function () {
-			$('.raynerNavBar').sideNav();
-			$('.parallax').parallax();
-			$('.carousel.carousel-slider').each(function() {
-				var view = $(this);
-				var firstImage = view.find('.carousel-item img').first();
-				var imageHeight = firstImage[0].height;
-				if (imageHeight > 0) {
-					view.css('height', imageHeight);
-				}
-				else {
-				//	view.css('height', 400);
-			}
-		});
-			$('.carousel').carousel({full_width: true});
-		});
-	}
-	angular.element(document).ready(function () {
-		$scope.init();
-	});
-
-})
-.controller('RootController',function($scope,$state){
+.controller('RootController',function($scope){
 	$scope.data='Root';
 
 	$scope.init = function(){
@@ -78,29 +29,6 @@ var app = angular.module('app',['ui.router'])
 })
 .controller('HomeController',function($scope){
 	$scope.data='Home';
-
-	$scope.init = function(){
-		$(function () {
-			$('.raynerNavBar').sideNav();
-			$('.parallax').parallax();
-			$('.carousel.carousel-slider').each(function() {
-				var view = $(this);
-				var firstImage = view.find('.carousel-item img').first();
-				var imageHeight = firstImage[0].height;
-				if (imageHeight > 0) {
-					view.css('height', imageHeight);
-				}
-				else {
-				//	view.css('height', 400);
-			}
-		});
-			$('.carousel').carousel({full_width: true});
-		});
-	}
-	angular.element(document).ready(function () {
-		$scope.init();
-	});
-
 });
 
 app.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider){
