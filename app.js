602191487389283ab66cb692b8c66996556af5be
raynerprogramming <raynerprogramming@gmail.com>
2017-06-09 12:40:55 -0500

    test

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 var app = angular.module('app',['ui.router'])
 .controller('FramingController',function($scope){
-$scope.data='Framing';
+	$scope.data='Framing';
+
+	$scope.init = function(){
+		$(function () {
+			$('.raynerNavBar').sideNav();
+			$('.parallax').parallax();
+			$('.carousel.carousel-slider').each(function() {
+				var view = $(this);
+				var firstImage = view.find('.carousel-item img').first();
+				var imageHeight = firstImage[0].height;
+				if (imageHeight > 0) {
+					view.css('height', imageHeight);
+				}
+				else {
+				//	view.css('height', 400);
+			}
+		});
+			$('.carousel').carousel({full_width: true});
+		});
+	}
+	angular.element(document).ready(function () {
+		$scope.init();
+	});
 })
 .controller('RootController',function($scope,$state){
 	$scope.data='Root';
@@ -8,7 +30,7 @@ $scope.data='Framing';
 })
 .controller('HomeController',function($scope){
 	$scope.data='Home';
-		$scope.init = function(){
+	$scope.init = function(){
 		$(function () {
 			$('.raynerNavBar').sideNav();
 			$('.parallax').parallax();
