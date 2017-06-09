7dfa3f8354d2bcaf6ce087cd7ac72ff853365a32
raynerprogramming <raynerprogramming@gmail.com>
2017-06-08 21:16:29 -0500

    test

--- app.js
+++ app.js
@@ -77,37 +77,68 @@ var app = angular.module('app',['ui.router'])
 	});
 
 });
+.controller('RootController',function($scope){
+	$scope.data='Root';
+
+	$scope.init = function(){
+		$(function () {
+			$('.raynerNavBar').sideNav();
+			$('.parallax').parallax();
+			$('.carousel.carousel-slider').each(function() {
+				var view = $(this);
+				var firstImage = view.find('.carousel-item img').first();
+				var imageHeight = firstImage[0].height;
+				if (imageHeight > 0) {
+					view.css('height', imageHeight);
+				}
+				else {
+				//	view.css('height', 400);
+			}
+		});
+			$('.carousel').carousel({full_width: true});
+		});
+	}
+	angular.element(document).ready(function () {
+		$scope.init();
+	});
+
+});
+
+.controller('framingController',function($scope){
+	$scope.data='Home';
+
+	$scope.init = function(){
+		$(function () {
+			$('.raynerNavBar').sideNav();
+			$('.parallax').parallax();
+			$('.carousel.carousel-slider').each(function() {
+				var view = $(this);
+				var firstImage = view.find('.carousel-item img').first();
+				var imageHeight = firstImage[0].height;
+				if (imageHeight > 0) {
+					view.css('height', imageHeight);
+				}
+				else {
+				//	view.css('height', 400);
+			}
+		});
+			$('.carousel').carousel({full_width: true});
+		});
+	}
+	angular.element(document).ready(function () {
+		$scope.init();
+	});
+
+});
 
 app.config(['$stateProvider', function($stateProvider){
 	$stateProvider
-	.state('root',{
-		url: '',
-		abstract: true,
-		views: {
-			'header': {
-				templateUrl: '/header.html',
-				controller: 'headerCtrl'
-			},
-			'footer':{
-				templateUrl: '/footer.html',
-				controller: 'footerCtrl'
-			}
-		}
+	.state('app',{
+		templateUrl: 'index.html',
+		controller: 'RootController'
 	})
-	.state('root.home', {
-		url: '/',
-		views: {
-			'container@': {
-				templateUrl: '/home.html'
-			}
-		}
-	})
-	.state('root.other', {
-		url: '/other',
-		views: {
-			'container@': {
-				templateUrl: '/other.html'
-			}
-		}
+	.state('app.home', {
+		templateUrl: 'home.html',
+		controller: 'HomeController'
 	});   
 }]);
